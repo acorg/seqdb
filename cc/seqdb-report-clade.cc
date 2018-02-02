c2424195259e20f6311af77cbee9181a8b980c31
Eugene Skepner <github@skepner.eu>
2018-02-02 17:55:28 +0100

    seqdb-report-clade improvements

--- cc/seqdb-report-clade.cc
+++ cc/seqdb-report-clade.cc
@@ -7,13 +7,15 @@ using namespace std::string_literals;
 
 // ----------------------------------------------------------------------
 
-constexpr const char* sUsage = " [options] <clade-name, e.g. DEL2017>\n";
+constexpr const char* sUsage = " [options] <clade-name, e.g. DEL2017 or all>\n";
 
 int main(int argc, char* const argv[])
 {
     try {
         argc_argv args(argc, argv, {
                 {"--db-dir", ""},
+                {"--lab", ""},
+                {"--flu", "", "A(H1N1), A(H3N2), B"},
                 {"-v", false},
                 {"--verbose", false},
                 {"-h", false},
@@ -24,10 +26,21 @@ int main(int argc, char* const argv[])
         }
         const bool verbose = args["-v"] || args["--verbose"];
         seqdb::setup_dbs(args["--db-dir"], verbose);
-        for (const auto entry_seq: seqdb::get()) {
-            if (entry_seq.seq().has_clade(args[0]))
-                std::cout << std::setw(60) << std::left << entry_seq.make_name()
-                          << " -- " << entry_seq.entry().virus_type() << ' ' << entry_seq.entry().lineage() << ' ' << entry_seq.entry().dates() << '\n';
+        if (args[0] == "all"s) {
+            for (const auto entry_seq: seqdb::get()) {
+                if ((!args["--lab"] || entry_seq.seq().has_lab(args["--lab"].str())) && (!args["--flu"] || entry_seq.entry().virus_type() == args["--flu"].str_view()))
+                    std::cout << std::setw(60) << std::left << entry_seq.make_name() << '\t' << entry_seq.seq().clades()
+                              << '\t' << entry_seq.entry().virus_type() << '\t' << entry_seq.entry().lineage() << '\t' << entry_seq.entry().dates()
+                              << '\t' << entry_seq.seq().lab() << '\n';
+            }
+        }
+        else {
+            for (const auto entry_seq: seqdb::get()) {
+                if (entry_seq.seq().has_clade(args[0]) && (!args["--lab"] || entry_seq.seq().has_lab(args["--lab"].str())) && (!args["--flu"] || entry_seq.entry().virus_type() == args["--flu"].str_view()))
+                    std::cout << std::setw(60) << std::left << entry_seq.make_name()
+                              << '\t' << entry_seq.entry().virus_type() << '\t' << entry_seq.entry().lineage() << '\t' << entry_seq.entry().dates()
+                              << '\t' << entry_seq.seq().lab() << '\n';
+            }
         }
         return 0;
     }
