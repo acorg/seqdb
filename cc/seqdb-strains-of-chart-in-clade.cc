c6b4bd29662d474b1cf7d272b518c01f6c9b21d1
Eugene Skepner <github@skepner.eu>
2018-09-06 16:02:31 +0200

    seqdb-strains-of-chart-in-clade

--- cc/seqdb-strains-of-chart-in-clade.cc
+++ cc/seqdb-strains-of-chart-in-clade.cc
@@ -8,13 +8,14 @@
 
 // ----------------------------------------------------------------------
 
-constexpr const char* sUsage = "<clade> <chart> [options]\n";
+constexpr const char* sUsage = " <clade> <chart> [options]\n";
 
 int main(int argc, char* const argv[])
 {
     using namespace std::string_literals;
     try {
         argc_argv args(argc, argv, {
+                {"--index-only", false},
                 {"--db-dir", ""},
                 {"-v", false},
                 {"--verbose", false},
@@ -25,19 +26,31 @@ int main(int argc, char* const argv[])
             throw std::runtime_error("Usage: "s + args.program() + sUsage + args.usage_options());
         }
         const bool verbose = args["-v"] || args["--verbose"];
+        const bool index_only = args["--index-only"];
         const std::string clade = args[0];
         seqdb::setup_dbs(args["--db-dir"], verbose ? seqdb::report::yes : seqdb::report::no);
         const auto& seqdb = seqdb::get();
         auto chart = acmacs::chart::import_from_file(args[1], acmacs::chart::Verify::None, report_time::No);
         auto antigens = chart->antigens();
         const auto per_antigen = seqdb.match(*antigens, chart->info()->virus_type());
+        bool first = true;
         for (auto [ag_no, entry] : acmacs::enumerate(per_antigen)) {
             if (entry && entry.seq().has_clade(clade)) {
                 if (!entry.seq().hi_name_present(antigens->at(ag_no)->full_name()))
                     throw std::runtime_error("ERROR: internal: matched sequence " + entry.entry().name() + " has no matched HI name for " + antigens->at(ag_no)->full_name());
-                std::cout << std::setw(5) << ag_no << ' ' << antigens->at(ag_no)->full_name() << ' ' << entry.seq().clades() << (antigens->at(ag_no)->reference() ? " REF" : "") << '\n';
+                if (index_only) {
+                    if (first)
+                        first = false;
+                    else
+                        std::cout << ',';
+                    std::cout << ag_no;
+                }
+                else
+                    std::cout << std::setw(5) << ag_no << ' ' << antigens->at(ag_no)->full_name() << ' ' << entry.seq().clades() << (antigens->at(ag_no)->reference() ? " REF" : "") << '\n';
             }
         }
+        if (index_only)
+            std::cout << '\n';
         return 0;
     }
     catch (std::exception& err) {
