4ada369941ad54b665a11c244132b64428a06319
Eugene Skepner <git@skepner.eu>
2019-03-14 13:27:18 +0100

    seqdb-amino-acid-stat

--- cc/seqdb-amino-acid-stat.cc
+++ cc/seqdb-amino-acid-stat.cc
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <algorithm>
+#include <array>
+#include <vector>
+
 #include "acmacs-base/argv.hh"
 #include "acmacs-base/string.hh"
 #include "acmacs-base/normalize.hh"
@@ -18,6 +22,10 @@ struct Options : public argv
     option<str>   end_date{*this, "end-date", desc{"isolated before date: YYYY-MM-DD"}};
 };
 
+constexpr const size_t NUM_POSITIONS = 600;
+// constexpr const size_t FIRST_AA = static_cast<size_t>('A');
+// constexpr const size_t NUM_AAS = static_cast<size_t>('Z') + 1 - FIRST_AA;
+
 int main(int argc, char* const argv[])
 {
     try {
@@ -32,10 +40,49 @@ int main(int argc, char* const argv[])
                 .filter_clade(string::upper(*opt.clade))
                 .filter_date_range(opt.start_date, opt.end_date)
                 ;
+
+        std::array<std::vector<std::pair<char, unsigned>>, NUM_POSITIONS> amino_acids_data;
         for (; iter != seqdb.end(); ++iter) {
+            const auto seq = (*iter).seq().amino_acids(true);
+            for (size_t pos = 0; pos < seq.size(); ++pos) {
+                auto& at = amino_acids_data[pos];
+                if (const auto found = std::find_if(std::begin(at), std::end(at), [aa=seq[pos]](const auto& entry) { return entry.first == aa; }); found != std::end(at))
+                    ++found->second;
+                else
+                    at.emplace_back(seq[pos], 1);
+            }
             ++num_sequences;
         }
         std::cout << "sequences: " << num_sequences << '\n';
+
+        for (size_t pos = 0; pos < amino_acids_data.size(); ++pos) {
+            if (amino_acids_data[pos].size() > 1) {
+                std::cout << std::setw(3) << (pos + 1);
+                for (const auto& [aa, num] : amino_acids_data[pos])
+                    std::cout << ' ' << aa << ':' << num;
+                std::cout << '\n';
+            }
+        }
+        
+        // std::array<std::array<unsigned, NUM_AAS>, NUM_POSITIONS> amino_acids_data; // amino_acids_data[pos][aa]
+        // std::for_each(std::begin(amino_acids_data), std::end(amino_acids_data), [](auto& entry) { entry.fill(0); }); // array requires initialization!
+
+        // for (; iter != seqdb.end(); ++iter) {
+        //     const auto seq = (*iter).seq().amino_acids(true);
+        //     for (size_t pos = 0; pos < seq.size(); ++pos)
+        //         ++amino_acids_data[pos][static_cast<size_t>(seq[pos]) - FIRST_AA];
+        //     ++num_sequences;
+        // }
+        // std::cout << "sequences: " << num_sequences << '\n';
+
+        // for (size_t pos = 0; pos < amino_acids_data.size(); ++pos) {
+        //     std::cout << std::setw(3) << pos;
+        //     for (size_t aa = 0; aa < NUM_AAS; ++aa) {
+        //         if (amino_acids_data[pos][aa])
+        //             std::cout << ' ' << static_cast<char>(aa + FIRST_AA + 1) << ':' << amino_acids_data[pos][aa];
+        //     }
+        //     std::cout << '\n';
+        // }
     }
     catch (std::exception& err) {
         std::cerr << "ERROR: " << err.what() << '\n';
